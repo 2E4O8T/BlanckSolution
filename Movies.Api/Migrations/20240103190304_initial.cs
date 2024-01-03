cb5fe8eb79e3b00d29c67cd1f4e27c140fe3043b
2E4O8T <benoit.froidevan@axa.fr>
2024-01-03 20:32:34 +0100

    Add project files.

---
+++ Movies.Api/Migrations/20240103190304_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Movies.Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Favorite = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ Movies.Api/Migrations/20240103192422_secondtime.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Movies.Api.Migrations
+{
+    public partial class secondtime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Favorite",
+                table: "Movies",
+                newName: "Owner");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Movies",
+                newName: "Genre");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Owner",
+                table: "Movies",
+                newName: "Favorite");
+
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Movies",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
