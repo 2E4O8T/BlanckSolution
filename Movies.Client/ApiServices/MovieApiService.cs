01e7d7ee6831abc23a3d40e7d64764bdb7f80dac
2E4O8T <benoit.froidevan@axa.fr>
2024-01-05 14:18:37 +0100

    Add-commit005

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,49 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Comics",
+                    Title = "Title",
+                    Rating = "9.2",
+                    ImageUrl = "images/src",
+                    ReleaseDate = DateTime.Now,
+                    Owner = "OncleBob"
+                });
+
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
