6dd8c513c1d1eb3810bba38e6afb9331268e47bd
2E4O8T <benoit.froidevan@axa.fr>
2024-01-08 22:27:53 +0100

    Add-commit007

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -1,7 +1,10 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Movies.Client.ApiServices
@@ -10,20 +13,65 @@ namespace Movies.Client.ApiServices
     {
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var movieList = new List<Movie>();
-            movieList.Add(
-                new Movie
-                {
-                    Id = 1,
-                    Genre = "Comics",
-                    Title = "Title",
-                    Rating = "9.2",
-                    ImageUrl = "images/src",
-                    ReleaseDate = DateTime.Now,
-                    Owner = "OncleBob"
-                });
+            // 1. Get Token from Identiy Server
+            // 2. Send request to protected API
+            // 3. Deserialize Object from MovieList
 
-            return await Task.FromResult(movieList);
+            // 1. Retrieve Api Crendentials
+            var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+
+                Scope = "movieAPI"
+            };
+
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            if (disco.IsError)
+            {
+                return null;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+            var apiClient = new HttpClient();
+
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:5001/api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            List<Movie> movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movieList;
+
+
+
+
+
+            //var movieList = new List<Movie>();
+            //movieList.Add(
+            //    new Movie
+            //    {
+            //        Id = 1,
+            //        Genre = "Comics",
+            //        Title = "Title",
+            //        Rating = "9.2",
+            //        ImageUrl = "images/src",
+            //        ReleaseDate = DateTime.Now,
+            //        Owner = "OncleBob"
+            //    });
+
+            //return await Task.FromResult(movieList);
         }
 
         public Task<Movie> GetMovie(string id)
